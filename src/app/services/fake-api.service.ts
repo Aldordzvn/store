b344c244c014000b42e6adb6fd999974913a76e9
Aldordzvn <aldordzvn2001@gmail.com>
2026-02-04 17:04:01 -0700

    Primer avance de service

---
+++ src/app/services/fake-api.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { Producto } from '../model/producto.model';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FakeApiService {
+  private readonly api = 'https://fakestoreapi.com/products';
+  private productos$ : BehaviorSubject<Producto[]> = new BehaviorSubject<Producto[]>([]);
+  constructor(private httpClient: HttpClient) { }
+
+  getProducts() : Observable<Producto[]>{
+    return this.httpClient.get<Producto[]>(this.api).pipe(
+      tap(res => this.productos$.next(res)),
+      tap(()=> console.log(this.api))
+    );
+  }
+
+  getProductsById(id: number) : Observable<Producto>{
+    return this.httpClient.get<Producto>(`${this.api}/${id}`);
+  }
+
+}
